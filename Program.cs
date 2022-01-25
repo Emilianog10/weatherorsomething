579638d1bf21edf8cd72f8d218ae52b66708342a
Emilianog10 <89863339+Emilianog10@users.noreply.github.com>
2022-01-25 09:42:01 -0800

    chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace weatherorsomething
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
