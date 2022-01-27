53c6f28f5554ec120d04c9e2fbb5963faac61b72
Emilianog10 <89863339+Emilianog10@users.noreply.github.com>
2022-01-27 12:22:35 -0800

    continue

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace weatherorsomething
 {
@@ -12,14 +13,7 @@ namespace weatherorsomething
 
 
         /// <summary>
-        /// This is bassicly the first part of the program where the user has to write start for the program to continue. 
-        /// 
-        /// TODO(jcollard: 2022-01-27): With comments, try to use the following format:
-        ///     "Given {variable-name} and {variable-name}, describe the action being performed. Then, returns {describe return-type}"
-        /// 
-        ///     Because this method does not have any parameters, you can skip the "Given" part. This methods comment should probably be something like this:
-        ///     "Prompts the user to enter a zip code and validates the text they entered is a 5 digit number. Then, returns the zip code they entered."
-        /// 
+        /// Prompts the user to enter a zip code and validates the text they entered is a 5 digit number. Then, returns the zip code they entered.
         /// </summary>
         /// 
         /// <returns></returns>
@@ -27,7 +21,7 @@ namespace weatherorsomething
         public static string NavigationPage()
         {
             // 1. Have a way to write start.
-            // 2. If start writen the start
+            // 2. If start written the start
             // 3. If not wait till it is. 
 
             return null;
@@ -37,13 +31,61 @@ namespace weatherorsomething
         // Maybe this method should be "ValidateZipcode(string input)"
         // This method checks that the input is 5 digits long and only contains numbers
         // It returns `true` if the zipcode is valid and `false` if the zipcode is not valid.
-        public static string Zipcode()
+
+        /// <summary>
+        /// Given a string to validate, validates it by cheking to see if it 5 charecters long and all chars are numbers. If valid returns true otherwise returns false.
+        /// </summary>
+        /// <param name="zipToValidate"></param>
+        /// <returns></returns>
+        public static string ValidateZipcode(string zipToValidate)
         {
             // TODO(jcollard 2022-01-27): Add step-by-step human readable algorithm
+            // 1. Takes zipToValidate and reads it
+            // 2. Makes sure that it is 5 charecters long.
+            // 3. Makes sure that it is only numbers. 
             return null;
         }
 
         // TODO(jcollard: 2022-01-27): Add remaining method stubs
+        /// <summary>
+        /// Prompts user to input either "1" for one day forecast or "3" for 3 day forcast. If not "1" or "3" return false. 
+        /// </summary>
+        /// <returns></returns>
+        public static string xnumberofdays()
+        {
+            // 1. Asks user if they want a 1 day forecast or a 3 day forcast. 
+            // 2. If they input 1 show todays forecast (currentDayWeather())
+            // 3. If they input 3 show 3 day forecast (forecast()) 
+            // 4. If not 1 or 3 return with invalid. 
+            return null;
+        }
+
+        public static void DisplayWeather()
+        {
+
+        }
+        /// <summary>
+        /// This method is called when the weather is needed. This method contains the forecast for the current day. 
+        /// </summary>
+        /// <param name="zipToValidate"></param>
+        /// <returns></returns>
+        public static List<string> forecast(string zipToValidate)
+        {
+            // 1. Take zipcode (zipToValidate) and input it into WeatherAPI
+            // 2. Save the info as a list. 
+            return null;
+        }
+        /// <summary>
+        /// This method is called when the weather is needed. This method contains the forecast for the next 3 days. 
+        /// </summary>
+        /// <param name="zipcode"></param>
+        /// <returns></returns>
+        public static string currentDayWeather(string zipcode)
+        {
+            // 1. Take zipcode (zipToValidate) and input it into WeatherAPI
+            // 2. Save the info as a list. 
+            return null;
+        }
 
     } // Feedback(jcollard 2022-01-27): Moved this curly bracket such that the methods were inside of the class. Methods must be within a class
 }
