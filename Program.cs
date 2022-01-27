cf3808b71c1032e0f16f889ad56d51e911f52144
Emilianog10 <89863339+Emilianog10@users.noreply.github.com>
2022-01-26 21:19:27 -0800

    feat: Add method stubs

--- Program.cs
+++ Program.cs
@@ -8,5 +8,28 @@ namespace weatherorsomething
         {
             Console.WriteLine("Hello World!");
         }
+
+        
+    }
+    /// <summary>
+    /// This is bassicly the first part of the program where the user has to write start for the program to continue. 
+    /// </summary>
+    /// 
+    /// <returns></returns>
+    /// 
+    public static string NavigationPage()
+    {
+        // 1. Have a way to write start.
+        // 2. If start writen the start
+        // 3. If not wait till it is. 
+        
+        return new Zipcode();
+    }
+
+     public static string Zipcode()
+    {
+        
+        
+        return new Zipcode();
     }
 }
